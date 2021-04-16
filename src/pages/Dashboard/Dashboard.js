68cecab9e2e8cf401790ad784770a764e513a617
Anthony Cardoze <anthonycardoze@gmail.com>
2021-04-16 09:43:00 -0500

    agregamos un h1 a la pagina de dashboard.

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -5,6 +5,7 @@ function Dashboard() {
   return (
     <DashboardLayout title="Dashboard">
       <h3>Dashboard page</h3>
+      <h1>Este cambio no esta correcto!!!</h1>
     </DashboardLayout>
   );
 }
