b04c404f1944fbe870922e6ad3260799dbdd80f2
Anthony Cardoze <anthonycardoze@gmail.com>
2021-04-21 13:11:11 -0500

    agregando libreria de fetch react query

--- src/functions/fetch/url.js
+++ src/functions/fetch/url.js
@@ -2,3 +2,7 @@ const authServerUrl = process.env.REACT_APP_AUTH_SERVER;
 
 export const urlLogin = `${authServerUrl}/api/auth/login`;
 export const urlValidated = `${authServerUrl}/api/auth/validate?id_app=`;
+
+export const urlGridUsuarios = `${process.env.REACT_APP_AUTH_SERVER}/api/auth/user-filtrados-v2`;
+
+export const urlGridWhereNotUsuarios = `${process.env.REACT_APP_AUTH_SERVER}/api/auth/user-whereNot-v2`;
