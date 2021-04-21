1543af26b60d5d174ad66bed6f22a384dfbab99c
Anthony Cardoze <anthonycardoze@gmail.com>
2021-04-21 12:58:25 -0500

    agregando tablas al los codigos

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Container({ children }) {
-  return <div style={{ width: "100%", height: "100%" }}>{children}</div>;
+function Container({ children, padding = 0 }) {
+  return (
+    <div style={{ width: "100%", paddingLeft: padding, paddingRight: padding }}>
+      {children}
+    </div>
+  );
 }
 
 export default Container;
--- src/pages/Configuraciones/bases/Grid.js
+++ src/pages/Configuraciones/bases/Grid.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 // plantillas
 
 import TituloDescripcion from "../../../components/TituloDescripcion/TituloDescripcion";
 import Toolbar from "../../../components/Toolbar/Toolbar";
+import Container from "../../../components/Container/Container";
+
+//componentes
+
+import DataTable from "../../../components/DataTable/databable";
+import BackdropSpinner from "../../../components/BackDrop/backDrop";
 
 function Grid() {
+  //local state
+
+  const [rows, setRows] = useState({});
+
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(50);
+  const [atrib, setAtrib] = useState("Id_Bases_Aeronaval");
+  const [order, setOrder] = useState("desc");
+
+  //plantillas de columnas
+
+  const columns = [
+    { tittle: "Base Ref", atributo: "Id_Bases_Aeronaval" },
+    { tittle: "Region", atributo: "region" },
+    { tittle: "Zona", atributo: "zona" },
+    { tittle: "Provincia", atributo: "provincia" },
+    { tittle: "Tipologia", atributo: "tipologia" },
+    { tittle: "Nombre", atributo: "Nombre_Base" },
+    { tittle: "Estatus", atributo: "Estatus_Base" },
+  ];
+
+  // funciones
+
+  const handleChangePage = (page) => setPage(page + 1);
+  const handleChangeLimit = (limit) => setLimit(limit);
+  const handleChangeAtrib = (atrib) => setAtrib(atrib);
+  const handleChangeOrder = (order) => setOrder(order);
+
+  //destructuracion de los datos de la fuente
+
+  const { results } = rows;
+  const { total } = rows;
+
   return (
     <>
       <TituloDescripcion
@@ -13,7 +53,20 @@ function Grid() {
         descripcion="Pantalla de modificacion y creacion de nuevas Bases."
       />
       <Toolbar type="bases" />
-      <div style={{ backgroundColor: "red", height: "100%" }}>Grid</div>
+      <Container padding={10}>
+        <DataTable
+          columns={columns}
+          total={total}
+          page={page}
+          limit={limit}
+          handleChangeLimit={handleChangeLimit}
+          handleChangePage={handleChangePage}
+          atrib={atrib}
+          order={order}
+          handleChangeAtrib={handleChangeAtrib}
+          handleChangeOrder={handleChangeOrder}
+        ></DataTable>
+      </Container>
     </>
   );
 }
